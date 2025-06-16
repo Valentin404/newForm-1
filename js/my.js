57c77cb5637bb6ca690b5c28c81ca1b991230214
Valentin404 <valik49717@gmail.com>
2025-06-16 18:22:05 +0300

    v1.000.0.1

--- js/my.js
+++ js/my.js
@@ -1,3 +1,8 @@
+const iti_0__country_listbox = document.querySelector('#iti-0__country-listbox');
+const iti__flagcontainer1 = document.querySelector('#iti__flag-container1');
+const iti__flag11 = document.querySelector('#iti__flag11');
+const registerBtn1 = document.querySelector('#registerBtn1');
+
 const checkbox0path = document.querySelector('#checkbox-0-path')
 const checkbox1path = document.querySelector('#checkbox-1-path')
 const checkbox2path = document.querySelector('#checkbox-2-path')
@@ -6,7 +11,33 @@ const checkbox0 = document.querySelector('#checkbox-0')
 const checkbox1 = document.querySelector('#checkbox-1')
 const checkbox2 = document.querySelector('#checkbox-2')
 
+const phonett1 = document.querySelector('#phone-tt-1');
+const firstName = document.querySelector('#firstName')
+const lastName = document.querySelector('#lastName')
+const email = document.querySelector('#email1')
 
+
+const isTestBTN = () => {
+    if (checkbox0path.checked) return registerBtn1.classList.add('wf-btn--disabled')
+    if (checkbox1path.checked) return registerBtn1.classList.add('wf-btn--disabled')
+    if (checkbox2path.checked) return registerBtn1.classList.add('wf-btn--disabled')
+    if (firstName.value.trim() === '') return registerBtn1.classList.add('wf-btn--disabled')
+    if (lastName.value.trim() === '') return registerBtn1.classList.add('wf-btn--disabled')
+
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!re.test(email.value)) return registerBtn1.classList.add('wf-btn--disabled')
+
+    if (phonett1.value.trim().length <= 8) return registerBtn1.classList.add('wf-btn--disabled')
+
+
+    registerBtn1.classList.remove('wf-btn--disabled')
+    isTestBTN()
+}
+
+phonett1.oninput = isTestBTN;
+lastName.oninput = isTestBTN;
+firstName.oninput = isTestBTN;
+email.oninput = isTestBTN;
 
 const changeCheckbox = (e) => {
     const is = e.target.checked;
@@ -14,7 +45,8 @@ const changeCheckbox = (e) => {
         ? "M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z"
         : "M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"
     );
-    
+    isTestBTN()
+
 }
 
 checkbox0.onchange = changeCheckbox
@@ -24,6 +56,7 @@ checkbox1.onchange = (e) => {
         ? "M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z"
         : "M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"
     );
+    isTestBTN()
 
 }
 checkbox2.onchange = (e) => {
@@ -32,20 +65,17 @@ checkbox2.onchange = (e) => {
         ? "M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z"
         : "M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"
     );
-    
+    isTestBTN()
+
 }
 
 
 // -----------------
-const iti_0__country_listbox = document.querySelector('#iti-0__country-listbox');
-const iti__flagcontainer1 = document.querySelector('#iti__flag-container1');
-const phonett1 = document.querySelector('#phone-tt-1');
-const iti__flag11 = document.querySelector('#iti__flag11');
 
 iti__flagcontainer1.onclick = (e) => {
     const t = e.target;
     const l = t.closest('.iti__country.iti__standard')
-    if(l){
+    if (l) {
         const countryCode = 'iti__' + l.getAttribute('data-country-code');
         const code = l.querySelector('.iti__dial-code')
         iti__flag11.className = 'iti__flag ' + countryCode
@@ -55,4 +85,13 @@ iti__flagcontainer1.onclick = (e) => {
 
 
     iti_0__country_listbox.classList.toggle('iti__hide')
+    isTestBTN()
+}
+
+
+
+
+registerBtn1.onclick = (e) => {
+    e.preventDefault()
+    window.location.href = 'https://thanks-en.zenstryde.store/'
 }
