c7ed5c912c0eb2076a887c7c9e1154151bde97e1
Valentin404 <valik49717@gmail.com>
2025-06-16 17:21:55 +0300

    v1

---
+++ __reg-form-custom/reg-form-custom.js
@@ -0,0 +1,20 @@
+const regFormContainerElementda7dce0fb02a40e5ac8c398580d5b50c = document.querySelector('reg-form-container[id="f31aee6c-2366-4936-bbac-0243ecb09a49"]');
+const regFormElementda7dce0fb02a40e5ac8c398580d5b50c = document.createElement('reg-form-plus');
+
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('first-title', 'Join the project Blazepeak Reaction and earn from 19400 CAD monthly, with an initial investment of only 350 CAD');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('subtitle', 'After registration wait for a call from manager, who will help you set up your account');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('input-label', 'false');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('last-name-placeholder', 'Enter your last name');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('email-placeholder', 'Enter your email');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('name-placeholder', 'Enter your first name');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('checkboxes', '["I agree to the Privacy Policy and Terms of Use","I agree to receive a call from an operator for clarification of information and conlsultation","Registration is only for CA citiziens"]');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('submit-button-text', 'Registration');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('excluded-countries', '[]');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('included-countries', '[]');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('lang', 'en');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('first-step', 'false');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('second-step', 'false');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('second-step-path', 'step2');
+regFormElementda7dce0fb02a40e5ac8c398580d5b50c.setAttribute('verify-phone', 'false');
+
+regFormContainerElementda7dce0fb02a40e5ac8c398580d5b50c?.appendChild(regFormElementda7dce0fb02a40e5ac8c398580d5b50c);
